df70e803d20eecbc4e478239d0b84217c8032a8b
DouglasChan <douglastschanto@gmail.com>
2020-06-30 16:45:11 -0400

    This is commit #1g. Used Punkt Sentence tokenizer? About to start part of speech tagging?

--- NLP_Processing_Landmarks.py
+++ NLP_Processing_Landmarks.py
@@ -1,7 +1,29 @@
 import nltk
+from nltk.tokenize.punkt import PunktSentenceTokenizer
+import time
 
 def processing_pages(text_list):
     for i in range(len(text_list)):
-        tokens = nltk.word_tokenize(text_list[i])
+    
+        custom_sent_tokenizer = PunktSentenceTokenizer() #Trained vs not? Leaving as default
         
-        print("Parts Of Speech: ", nltk.pos_tag(tokens))
+        tokenized = custom_sent_tokenizer.tokenize(text_list[i])
+        
+        #for item in tokenized:
+        #    print(item)
+        
+        for item in tokenized:
+            time.sleep(0.1)
+            print(item)
+            print(len(tokenized))
+        
+        #print(tokenized)
+        #for j in tokenized:
+        #    print(j)
+    
+        #tokens = nltk.word_tokenize(text_list[i])
+        
+        #print("Parts Of Speech: ", nltk.pos_tag(tokens))
+        
+#if __name__ == '__main__':
+    
