522f392f9ba10fce3462d497107d7d345cedc2ae
DouglasChan <douglastschanto@gmail.com>
2020-07-31 11:25:54 -0400

    This is commit #1l. Changing from selecting cell data IP address to using phone's coordinates, assuming csv has gone from phone to computer.

--- Location_Checker.py
+++ Location_Checker.py
@@ -1,22 +1,49 @@
 import requests
 import time
+import csv
+from os import getcwd, listdir
+\
+
+def find_csv_filenames(path_to_dir, suffix = '.csv'):
+    filenames = listdir(path_to_dir)
+    return [filename for filename in filenames if filename.endswith(suffix)]
 
 def display_ip():
 
     coordinates_list = []
     
-    counter = 0
+    path = getcwd() # Current working directory
     
-    while counter < 15:
+    filenames = find_csv_filenames(path)
+    for name in filenames:
+        print(name)
+    
+    #Reading in CSV data
+    
+    #Take given csv file
+    #Ideally later would want all csv files combined
+    
+    #Output list of latitudes and longiudes
+    
+    
+    #counter = 0
+    
+    
+    
+    #while counter < 1000000:
 
-        ip_request = requests.get('https://get.geojs.io/v1/ip.json')
-        my_ip = ip_request.json()['ip']
-        geo_request = requests.get('https://get.geojs.io/v1/ip/geo/' +my_ip + '.json')
-        geo_data = geo_request.json()
-        print({'latitude': geo_data['latitude'], 'longitude': geo_data['longitude']})
-        coordinates_list.append(tuple((geo_data['latitude'],geo_data['longitude'])))
+        # Code below isn't great -- since takes the geolocation of the cell tower and not anything actually useful. 
+
+        #ip_request = requests.get('https://get.geojs.io/v1/ip.json')
+        #my_ip = ip_request.json()['ip']
+        #geo_request = requests.get('https://get.geojs.io/v1/ip/geo/' +my_ip + '.json')
+        #geo_data = geo_request.json()
+        #print({'latitude': geo_data['latitude'], 'longitude': geo_data['longitude']})
+        #coordinates_list.append(tuple((geo_data['latitude'],geo_data['longitude'])))
         
-        counter += 1
+    #    counter += 1
+    #    print(counter)
+    #    time.sleep(15)
     
     print(coordinates_list)
     return coordinates_list
@@ -26,4 +53,4 @@ if __name__ == '__main__':
 
 
 #Ref
-#
+#1. https://note.nkmk.me/en/python-os-getcwd-chdir/#:~:text=Get%20the%20current%20working%20direcory%3A%20os.,-getcwd()&text=getcwd()-,os.,the%20result%20with%20print()%20.&text=getcwd%20stands%20for%20%22get%20current%20working%20directory%22.
